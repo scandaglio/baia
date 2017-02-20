898d3d46582fa1271d2f0249e03e4fed72330644
uf0 <scnepa222@gmail.com>
2017-02-20 20:09:47 +0100

    bootstrap project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name baiaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the baiaApp
+ */
+angular.module('baiaApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
